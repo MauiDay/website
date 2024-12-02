3e6789a8efc6d9debfff2fdbd5c7be6b13b54c2c
sthewissen <github@thewissen.io>
2024-12-02 21:30:10 +0100

    Event configs

---
+++ src/data/configs/london-config.js
@@ -0,0 +1,14 @@
+export default {
+    name: "",
+    sessionizeId: "8cknf5zh",
+    location: "",
+    locationAddress: "",
+    sponsors: [
+        {
+            name: "JetBrains",
+            logo: "",
+            url: "",
+        },
+    ],
+  };
+  
