5549faaf4acca315a58104aa9639453b38c7786b
Gerald Versluis <gerald.versluis@microsoft.com>
2024-12-04 09:02:15 +0100

    Switch to London config

--- src/data/configs/london-config.mjs
+++ src/data/configs/london-config.mjs
@@ -1,34 +1,18 @@
 export default {
     name: "",
     sessionizeId: "8cknf5zh",
-    location: "",
-    ticketUrl: "",
-    locationAddress: "",
+    location: "Microsoft Reactor London",
+    ticketUrl: "https://mauiday-london-2025.eventbrite.nl",
+    locationAddress: `
+        <b>Microsoft Reactor London</b><br/>
+        70 Wilson Street, London, EC2A 2DB<br/>
+        United Kingdom
+    `,
     sponsors: [
         {
             name: "Microsoft",
             logo: "microsoft",
             url: "https://www.microsoft.com",
-        },
-        {
-            name: "Syncfusion",
-            logo: "syncfusion",
-            url: "https://syncfusion.com/",
-        },
-        {
-            name: "Grial UI Kit",
-            logo: "grial",
-            url: "https://grialkit.com/",
-        },
-        {
-            name: "Telerik",
-            logo: "telerik",
-            url: "https://www.telerik.com/maui-ui",
-        },
-        {
-            name: "JetBrains",
-            logo: "jetbrains",
-            url: "https://jetbrains.com/",
         },
         {
             name: "Sessionize",
