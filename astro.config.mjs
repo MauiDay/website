8802d68fbe44699430ca90f3b70ca3abfead7dd9
sthewissen <github@thewissen.io>
2024-12-04 20:15:27 +0100

    gone with subdomain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  base: 'https://www.mauiday.net', // Once DNS is in place, empty this.
   site: 'https://www.mauiday.net',
   integrations: [tailwind(), sitemap()]
 });
