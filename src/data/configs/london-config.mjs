d3940fc6efcf09771ed269ed9f0ec4b00b803c1f
Tony Edwards <tonyedwardspz@users.noreply.github.com>
2025-02-06 18:41:00 +0000

    Updates venue to Just Eat

--- src/data/configs/london-config.mjs
+++ src/data/configs/london-config.mjs
@@ -6,13 +6,13 @@ export default {
     hasCfs: false,
     hasAnnouncedSpeakers: true,
     hasAnnouncedSchedule: false,
-    location: "Microsoft Reactor London",
-    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4965.064456132526!2d-0.08681318739155501!3d51.52179730953346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761cae412fffff%3A0xe56540ed5d9ddc24!2sMicrosoft%20Reactor%20London!5e0!3m2!1sen!2snl!4v1733307998847!5m2!1sen!2snl",
+    location: "Just Eat London",
+    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158923.9359208257!2d-0.2740424197835111!3d51.509794375987724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761b52e612c625%3A0x6b62dacd84c1270c!2sJust%20Eat!5e0!3m2!1sen!2suk!4v1738867136142!5m2!1sen!2suk",
     ticketUrl: "https://mauiday-london-2025.eventbrite.nl",
     locationNote:"",
     locationAddress: `
-        <b>Microsoft Reactor London</b><br/>
-        70 Wilson Street, London, EC2A 2DB<br/>
+        <b>Just Eat London</b><br/>
+        Fleet Place House, 2 Fleet Place, London, EC4M 7RF<br/>
         United Kingdom
     `,
     codeOfConductMain: `<a class="text-brand-extra hover:underline" href="mailto:gerald@verslu.is">Gerald Versluis</a> (not part of the London organizers, but involved)`,
