8192a9d75f218816d4d95ca0eccf6936619b339a
Tony Edwards <tonyedwardspz@users.noreply.github.com>
2025-12-03 20:32:26 +0000

    Updates config

--- src/data/configs/london-config.mjs
+++ src/data/configs/london-config.mjs
@@ -1,45 +1,51 @@
 export default {
     name: "",
     sessionizeId: "9vn5sw3z",
-    sessionizeCfsUrl: "https://sessionize.com/maui-day-london-2025/",
-    headline: "Next event: March 14, 2025 in London!",
-    hasCfs: false,
-    hasAnnouncedSpeakers: true,
-    hasAnnouncedSchedule: true,
-    location: "Just Eat London",
-    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158923.9359208257!2d-0.2740424197835111!3d51.509794375987724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761b52e612c625%3A0x6b62dacd84c1270c!2sJust%20Eat!5e0!3m2!1sen!2suk!4v1738867136142!5m2!1sen!2suk",
-    ticketUrl: "https://mauiday-london-2025.eventbrite.nl",
+    sessionizeCfsUrl: "https://sessionize.com/maui-day-london-2026",
+    headline: "Next event: February 6th, 2026 in London!",
+    hasCfs: true,
+    hasAnnouncedSpeakers: false,
+    hasAnnouncedSchedule: false,
+    location: "NewDay",
+    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2481.7075286107192!2d-0.12333730000000002!3d51.536924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761b3daaaaaaab%3A0xf88c121c552af668!2sNewDay!5e0!3m2!1sen!2sde!4v1764791780638!5m2!1sen!2sde",
+    ticketUrl: "https://e.runevents.net/maui-day-london-2026/checkout",
     locationNote:"",
     locationAddress: `
-        <b>Just Eat London</b><br/>
-        Fleet Place House, 2 Fleet Place, London, EC4M 7RF<br/>
+        <b>NewDay</b><br/>
+        7 Handyside St, London, N1C 4DA<br/>
         United Kingdom
     `,
     codeOfConductMain: `<a class="text-brand-extra hover:underline" href="mailto:gerald@verslu.is">Gerald Versluis</a> (not part of the London organizers, but involved)`,
     codeOfConductSecondary: `<a class="text-brand-extra hover:underline" href="mailto:Tony.edwards@gmail.com">Tony Edwards</a> (note: part of the London crew)`,
     sponsors: [
-        {
-            name: "Syncfusion",
-            logo: "syncfusion",
-            url: "https://syncfusion.com/",
-            boost: true,
-        },       
-        {
-            name: "Telerik",
-            logo: "progress",
-            url: "https://www.telerik.com/maui-ui",
-            boost: true,
-        },
-        {
-            name: "Sentry",
-            logo: "sentry",
-            url: "https://sentry.io",
-            boost: true,
-        },
+        // {
+        //     name: "Syncfusion",
+        //     logo: "syncfusion",
+        //     url: "https://syncfusion.com/",
+        //     boost: true,
+        // },       
+        // {
+        //     name: "Telerik",
+        //     logo: "progress",
+        //     url: "https://www.telerik.com/maui-ui",
+        //     boost: true,
+        // },
+        // {
+        //     name: "Sentry",
+        //     logo: "sentry",
+        //     url: "https://sentry.io",
+        //     boost: true,
+        // },
         {
             name: "Microsoft",
             logo: "microsoft",
             url: "https://www.microsoft.com",
+            boost: false,
+        },
+        {
+            name: "NewDay",
+            logo: "newday",
+            url: "https://newday.co.uk/",
             boost: true,
         },
         {
@@ -47,27 +53,20 @@ export default {
             logo: "sessionize",
             url: "https://sessionize.com",
             boost: false,   
-        },
-        {
-            name: "JetBrains",
-            logo: "jetbrains",
-            url: "https://www.jetbrains.com/",
-        },
-        {
-            name: "Just Eat",
-            logo: "justeat",
-            url: "https://www.just-eat.co.uk/",
-        },
-        // {
-        //     name: "Grial Kit",
-        //     logo: "grial",
-        //     url: "https://grialkit.com/",
-        // },
-        {
-            name: "UX Divers",
-            logo: "ux-divers",
-            url: "https://uxdivers.com/",
         }
+        // ,
+        // {
+        //     name: "JetBrains",
+        //     logo: "jetbrains",
+        //     url: "https://www.jetbrains.com/",
+        //     boost: false,
+        // }
+        //,
+        // {
+        //     name: "UX Divers",
+        //     logo: "ux-divers",
+        //     url: "https://uxdivers.com/",
+        // }
     ],
   };
-  
+
