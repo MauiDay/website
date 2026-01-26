6008f417a0a4c71adaf332a1db2f3b4c9d2d272d
Tony Edwards <tonyedwardspz@users.noreply.github.com>
2026-01-26 20:28:27 +0000

    tickets / sessionize / map ready

---
+++ src/data/configs/krakow-config.mjs
@@ -0,0 +1,72 @@
+export default {
+    name: "",
+    sessionizeId: "0owlbzma",
+    sessionizeCfsUrl: "https://sessionize.com/maui-day-krakow-2026",
+    headline: "Next event: May 29th, 2026 in Kraków!",
+    hasCfs: true,
+    hasAnnouncedSpeakers: true,
+    hasAnnouncedSchedule: false,
+    location: "Politechnika Krakowska",
+    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2560.760429914753!2d19.940189276500035!3d50.07204811453742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47165b04a4a0d5bf%3A0x41a815e1860a19eb!2sCracow%20University%20of%20Technology!5e0!3m2!1sen!2suk!4v1769458350879!5m2!1sen!2suk",
+    ticketUrl: "https://www.eventbrite.nl/e/net-maui-day-krakow-2026-tickets-1981602150375",
+    locationNote:"",
+    locationAddress: `
+        <b>Politechnika Krakowska</b><br/>
+        Warszawska 24, Kraków<br/>
+        Poland
+    `,
+    codeOfConductMain: `<a class="text-brand-extra hover:underline" href="mailto:gerald@verslu.is">Gerald Versluis</a> (not part of the London organizers, but involved)`,
+    codeOfConductSecondary: `<a class="text-brand-extra hover:underline" href="mailto:tony@mauiday.net">Tony Edwards</a> (note: part of the London crew)`,
+    sponsors: [
+        // {
+        //     name: "Syncfusion",
+        //     logo: "syncfusion",
+        //     url: "https://syncfusion.com/",
+        //     boost: true,
+        // },       
+        // {
+        //     name: "Telerik",
+        //     logo: "progress",
+        //     url: "https://www.telerik.com/maui-ui",
+        //     boost: true,
+        // },
+        // {
+        //     name: "Sentry",
+        //     logo: "sentry",
+        //     url: "https://sentry.io",
+        //     boost: true,
+        // },
+        {
+            name: "Microsoft",
+            logo: "microsoft",
+            url: "https://www.microsoft.com",
+            boost: false,
+        },
+        // {
+        //     name: "NewDay",
+        //     logo: "newday",
+        //     url: "https://newday.co.uk/",
+        //     boost: true,
+        // },
+        {
+            name: "Sessionize",
+            logo: "sessionize",
+            url: "https://sessionize.com",
+            boost: false,   
+        }
+        // ,
+        // {
+        //     name: "JetBrains",
+        //     logo: "jetbrains",
+        //     url: "https://www.jetbrains.com/",
+        //     boost: false,
+        // }
+        //,
+        // {
+        //     name: "UX Divers",
+        //     logo: "ux-divers",
+        //     url: "https://uxdivers.com/",
+        // }
+    ],
+  };
+
