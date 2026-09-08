7dbd63b68d04b34fd36bd8ba522e92c880e16ef1
Tony Edwards <tonyedwardspz@users.noreply.github.com>
2026-09-08 09:36:28 +0100

    Updates skopje addy (#80)
    * Update venue
    
    * Updates Skopje venue
    
    Also adds a config variable for doors open time

--- src/data/configs/default-config.mjs
+++ src/data/configs/default-config.mjs
@@ -18,6 +18,7 @@ export default {
         Holzmarkt 2, 50676 Köln<br/>
         Germany
     `,
+    doorsOpenTime: "8:30am",
     codeOfConductMain: `<a class="text-brand-extra hover:underline" href="mailto:gerald@verslu.is">Gerald Versluis</a>`,
     codeOfConductSecondary: `<a class="text-brand-extra hover:underline" href="mailto:Anjuli.Jhakry@hotmail.com">Anjuli Jhakry</a> (not part of the organization crew)`,
     sponsors: [
--- src/data/configs/skopje-config.mjs
+++ src/data/configs/skopje-config.mjs
@@ -5,9 +5,9 @@ export default {
     headline: "Event Date: September 17th 2026 in Skopje!",
     hasCfs: true,
     hasAnnouncedSpeakers: true,
-    hasAnnouncedSchedule: false,
-    location: "Faculty of Computer Science & Engineering",
-    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28001.339626201563!2d21.408426604766383!3d42.0087091014232!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13541443605aa4ab%3A0x33d56647e5b87264!2sFaculty%20of%20Computer%20Science%20%26%20Engineering!5e0!3m2!1sen!2suk!4v1782216987207!5m2!1sen!2suk",
+    hasAnnouncedSchedule: true,
+    location: "INNOFEIT EDIH",
+    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2964.8102907154143!2d21.408280899999998!3d42.0043468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1354156a87e09fdd%3A0x6bf62a9cae5ef17f!2sINNOFEIT!5e0!3m2!1sen!2suk!4v1788812191755!5m2!1sen!2suk",
     mapsLinkUrl: "https://maps.app.goo.gl/qUjmgxXHBQTZYQSD8",
     ticketUrl: "https://www.eventbrite.nl/e/net-maui-day-skopje-2026-tickets-1992309951697",
     locationNote: "",
@@ -16,10 +16,11 @@ export default {
     locationTransit: 'Bus Stop 5 minutes walk',
     locationCapacity: '70',
     locationAddress: `
-        Ruger Boskovik 16,<br/>
+        Ruger Boskovik 18,<br/>
         Skopje 1000,<br/>
         North Macedonia
     `,
+    doorsOpenTime: "9:00am",
     codeOfConductMain: `<a class="text-brand-extra hover:underline" href="mailto:gerald@verslu.is">Gerald Versluis</a>`,
     codeOfConductSecondary: `<a class="text-brand-extra hover:underline" href="mailto:tony@mauiday.net">Tony Edwards</a>`,
     sponsors: [
